feae8837ac70fd6582ab85cf99cb4f2caaf2a6fe
Consuelo Ascanio <consueloascanio@gmail.com>
2021-09-12 19:40:05 -0300

    DataTable integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -31,7 +31,7 @@ const BarChart = () => {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}'/sales/success-by-seller'`)
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
             .then(response => {
                 const data = response.data as SaleSuccess[];
                 const myLabels = data.map(x => x.sellerName);
